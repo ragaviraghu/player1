a47d53210cb64b0d5b9b55add7d5e57c60a4eb24
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-03-31 08:28:04 -0700

    Create append.cpp

---
+++ append.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+string S;
+cout<<"Enter the string : "<<endl;
+cin>>S;
+cout<<"The appeed string is : "<<S.append(".")<<endl;
+return 0;
+}
