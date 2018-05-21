50a616031975bb70c9333e518a07a46f4afcf4d9
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-05-20 22:42:19 -0700

    Update isomorphic.cpp

--- isomorphic.cpp
+++ isomorphic.cpp
@@ -11,13 +11,19 @@ m=a.length();
 n=b.length();
 for(int i=0;i<m;i++)
 {
-if(a[i]==a[i+1])
+for(int j=i+1;j<m;j++)
+{
+if(a[i]==a[j])
 count1++;
+}
 }
 for(int i=0;i<n;i++)
 {
-if(b[i]==b[i+1])
-count2++;
+for(int j=i+1;j<n;j++)
+{
+if(b[i]==b[j])
+count1++;
+}
 }
 if(count1==count2)
 {
