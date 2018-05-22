b2e5895d49490f7488375cd00e2c86a2eb81c834
ragaviraghu <36372478+ragaviraghu@users.noreply.github.com>
2018-05-22 14:06:59 +0530

    Create string_one.cpp

---
+++ string_one.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+string a,b;
+int n,m,count=0;
+cout<<"Enter the strings : "<<endl;
+cin>>a>>b;
+n=a.length();
+m=b.length();
+if(m==n)
+{ 
+for(int i=0;i<n;i++)
+{
+if(a[i]!=b[i])
+{
+count++;
+}
+}
+if(count==1)
+{
+cout<<"Yes(strings differ by only one character)"<<endl;
+}
+else
+{
+cout<<"No(strings doesn't differ dy only one character)"<<endl;	
+}
+}
+else
+{
+cout<<"No"<<endl;
+}
+return 0;
+}
